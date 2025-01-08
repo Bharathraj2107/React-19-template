cb36f46dc07d13874db71b3e028b4ee9fe3dbcd3
dganesh22 <dganesh2022@gmail.com>
2025-01-08 10:19:01 +0530

    react-19-template

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import lib - import library from 'source'
+import React from 'react'
+
+// component body
+function App() {
+    return (
+        <div>
+            <h1>Welcome to ReactJS</h1>
+        </div>
+    )
+}
+
+
+// export component
+export default App
